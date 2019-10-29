2d389d9134e3e1dc0dbabba240720fec1434dcb6
Sam Guyer <sam.guyer@gmail.com>
2019-10-29 10:01:56 -0400

    Search algorithms for lecture 16

---
+++ Lecture16.py
@@ -0,0 +1,33 @@
+import random
+import time
+
+def linear_search(lookingfor, vals):
+    return False
+
+def binary_search(lookingfor, vals):
+    return False
+
+# --- Main program ------------------------
+s = input('Enter list length: ')
+size = int(s)
+
+# -- Make a sorted list of random numbers
+numlist = [ 0 for _ in range(int(size))]
+total = 0
+for i in range(size):
+    total = total + int(random.random() * 5)
+    numlist[i] = total
+
+# -- Run and time one of the algorithms
+done = False
+while not done:
+    s = input('Enter a number to find: ')
+    if s == "quit" or s == "done":
+        done = True
+    else:
+        val = int(s)
+        t1 = time.time()
+        found = linear_search(val, numlist)
+        t2 = time.time()
+        print("Result is " + str(found))
+        print("Time: " + str(t2 - t1))
